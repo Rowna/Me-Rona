d4b364dbaa27e4cffbcde87bf845db94159a24a6
Rowna <rowan.kh1@gmail.com>
2021-09-09 16:41:35 +0200

    p: Refactoring Background & Pics

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+// $(window).on("scroll", function() {
+//     if($(window).scrollTop() > 50) {
+//         $(".header").addClass("active");
+//     } else {
+//         //remove the background property so it comes transparent again (defined in your css)
+//        $(".header").removeClass("active");
+//     }
+// });
+
+// window.alert('Hallo Welt!');
+// console.log("index.js geladen.")
